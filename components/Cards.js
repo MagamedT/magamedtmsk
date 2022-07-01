54090998774797080b6da0af5c546c786a76a380
Magamed <taimeskhanov@gmail.com>
2022-07-01 04:38:37 +0200

    responsive cards change

--- components/Cards.js
+++ components/Cards.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function Card({texte , link}) {
     return (
       <Link href = {link}>
-      <div className="flex flex-col m-4 scale-100 items-center justify-center border-solid border-2 border-[#9234ea] text-black h-40 w-40  rounded-lg shadow-xl hover:shadow-[#9234ea] hover:shadow-sm hover:scale-105 transition-all duration-300 cursor-pointer">
+      <div className="w-full flex flex-col m-4 scale-100 items-center justify-center border-solid border-2 border-[#9234ea] text-black rounded-lg shadow-xl hover:shadow-[#9234ea] hover:shadow-sm hover:scale-105 transition-all duration-300 cursor-pointer md:h-40 md:w-40 ">
           
         <p className="text-2xs text-center text-white p-4 font-medium italic">{texte}</p> 
           
