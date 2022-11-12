063346ebe03e247374493a3f500bcedc18a28c38
MagamedT <taimeskhanov@gmail.com>
2022-11-12 02:06:22 +0100

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 // import { MDXProvider } from '@mdx-js/react'
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 
 function MyApp({ Component, pageProps }) {
   return (
-          <Component {...pageProps} />    
+      <>
+          <Component {...pageProps} />
+          <Analytics />
+      </>  
   )
 }
 
